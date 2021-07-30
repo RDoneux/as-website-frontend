8ba4061a3cb95bb3730d3c4ba7f03d2df6131a96
R.Doneux <robertdoneux@gmail.com>
2021-07-30 17:06:09 +0100

    test display updates

--- src/pages/testimony/Testimonies.tsx
+++ src/pages/testimony/Testimonies.tsx
@@ -9,18 +9,50 @@ interface TestimoniesProps {
 
 }
 
-class Testimonies extends React.Component<TestimoniesProps> {
+interface TestimonyEntryState {
+    loading: boolean
+}
+
+class Testimonies extends React.Component<TestimoniesProps, TestimonyEntryState> {
+
+    constructor(props: TestimoniesProps) {
+        super(props)
+        this.state = {
+            loading: true
+        }
+    }
 
     render() {
         const testimonies: testimony[] = data
+        const loading = this.state.loading
         return (
-            <div className={Styles.container}>
+            <div>
                 {testimonies.map((testimony: testimony, i) => (
+                    <div className={Styles.container}>
+                        <div className={Styles.image_wrapper}>
+                            <img
+                                style={{ objectFit: 'cover' }}
+                                className={(loading) ? Styles.image_loading : Styles.image_loaded}
+                                loading="lazy"
+                                src={testimony.image_url}
+                                onLoad={() => { this.setState({ loading: false }) }}
+                                alt=""
+                            />
+                        </div>
+                        <div>
+                            <h2 className={Styles.heading}>{testimony.author}</h2>
+                            <p className={Styles.paragraph}>"{testimony.text}" - {testimony.date}</p>
+                        </div>
+                    </div>
+                ))}
+                {/* {testimonies.map((testimony: testimony, i) => (
                     <TestimonyEntry
                         key={i}
                         testimony={testimony}
                     />
-                ))}
+                ))} */}
+
+
 
             </div>
 
